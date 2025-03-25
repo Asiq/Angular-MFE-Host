ef12ebe25526fa858f62d7ce6e19e5faa01d1645
Asiq <s.asiqhussain@gmail.com>
2025-03-25 22:47:55 +0530

    Initail commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4300/remoteEntry.js',
+        exposedModule: './AppComponent',
+      })
+        .then((m) => m.AppComponent)
+        .catch((err) => console.log(err)),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4300/remoteEntry.js',
+        exposedModule: './LoginComponent',
+      })
+        .then((m) => m.LoginComponent)
+        .catch((err) => console.log(err)),
+  },
+  {
+    path: 'analytics',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4300/remoteEntry.js',
+        exposedModule: './UserAnalyticsComponent',
+      })
+        .then((m) => m.UserAnalyticsComponent)
+        .catch((err) => console.log(err)),
+  },
+  //   {
+  //     path: 'login',
+  //     loadComponent: () =>
+  //       import('microfrontend_remote/Login').then((m) => m.Login),
+  //   },
+];
---
+++ src/declarations.d.ts
@@ -0,0 +1,1 @@
+declare module 'microfrontend_remote/Login';
