8578aca292e4e15057a173204274d3bb98a7fa7f
Asiq <s.asiqhussain@gmail.com>
2025-03-26 07:12:52 +0530

    Added Material sidenav and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'microfrontend_host';
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const {
 } = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-  // name: "microfrontend_host",
+  name: "microfrontend_host",
 
   remotes: {
     microfrontend_remote:
@@ -19,55 +19,3 @@ module.exports = withModuleFederationPlugin({
     }),
   },
 });
-
-/*
-const { ModuleFederationPlugin } = require("webpack").container;
-const mf = require("@angular-architects/module-federation/webpack");
-const share = mf.share;
-
-module.exports = {
-  output: {
-    publicPath: "auto",
-    scriptType: "text/javascript",
-    library: {
-      type: "module", // Ensures remoteEntry.js is treated as an ES module
-    },
-  },
-  optimization: {
-    runtimeChunk: false,
-  },
-  experiments: {
-    outputModule: true, // Enables ES module output
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "microfrontend_host",
-      remotes: {
-        microfrontend_remote:
-          "microfrontend_remote@http://localhost:4300/remoteEntry.js",
-      },
-      shared: share({
-        "@angular/core": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-        "@angular/common": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-        "@angular/router": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-        "@angular/common/http": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-      }),
-    }),
-  ],
-};*/
